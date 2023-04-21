dcf5248acdd2133ff4590fe6a2e56987ffa15718
m.bagheri <bagheri.mohammadreza10@gmail.com>
2023-04-21 17:32:36 +0330

    inject services in application layer

--- src/application/Usecases/DependencyInjection.cs
+++ src/application/Usecases/DependencyInjection.cs
@@ -1,10 +1,18 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DomainDrivenDesignArchitucture.Application.Usecases;
 public static class DependencyInjection
 {
     public static IServiceCollection AddUseCase(this IServiceCollection services)
     {
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining(AssemblyReferences.ASSEMBLY_TYPE));
+
+        services.AddAutoMapper(AssemblyReferences.ASSEMBLY_TYPE);
+
+        services.AddValidatorsFromAssembly(AssemblyReferences.ASSEMBLY);
+
         return services;
     }
 }
